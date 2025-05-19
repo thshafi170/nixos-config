2d2b157dbfd62886015c9f65cf12f175d9d613f7
Shafa'el Zmeyev <shafael170@gmail.com>
2025-05-19 21:45:59 +0600

    Im dumb 2 electric boogaloo

--- home.nix
+++ home.nix
@@ -18,24 +18,25 @@
       nix-clean = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system && sudo nix-collect-garbage && sudo nixos-rebuild boot";
     };
     functions = {
-      fish_greeting = ''
-        function fish_greeting
-          fastfetch
-        end
-      '';
-      pythonEnv = ''
-        function pythonEnv --description 'start a nix-shell with the given python packages' --argument pythonVersion
-        if set -q argv[2]
-          set argv $argv[2..-1]
-        end
-      
-        for el in $argv
-          set ppkgs $ppkgs "python"$pythonVersion"Packages.$el"
-        end
-      
-        nix-shell -p $ppkgs
-        end
-      '';
+      fish_greeting = {
+        description = "Start fastfetch at launch";
+        body = "fastfetch";
+      };
+      pythonEnv = {
+        description = "Start a nix-shall with given python packages";
+        argumentNames = [ "pythonVersion" ];
+        body = ''
+          set -l pythonVersion $argv[1]
+          set -e argv[1]
+
+          set -l ppkgs
+          for el in $argv
+            set ppkgs $ppkgs python${pythonVersion}Packages.$el
+          end
+
+          nix-shell -p $ppkgs
+        '';
+      };
     };
   };
 
